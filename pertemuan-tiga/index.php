770572e672374e94938a61e24e08a21f4b60a566
zackster <mochzackyaja@gmail.com>
2024-10-25 18:37:25 +0700

    Update index.php

--- pertemuan-tiga/index.php
+++ pertemuan-tiga/index.php
@@ -18,7 +18,7 @@ include("list_menu.php");
 <body class="container">
      <nav class="navbar bg-body-tertiary">
           <div class="container-fluid">
-               <a class="navbar-brand">Latihan Program PBW</a>
+               <a class="navbar-brand">Latihan PBW - Pertemuan Tiga</a>
                <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
