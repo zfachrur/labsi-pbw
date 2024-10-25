52a6c41c49e3846bf3fdfed85034105067132425
zackster <mochzackyaja@gmail.com>
2024-10-25 18:57:40 +0700

    Update index.php

--- pertemuan-tiga/index.php
+++ pertemuan-tiga/index.php
@@ -26,7 +26,7 @@ include("list_menu.php");
           </div>
      </nav>
      <div class="d-flex justify-content-center align-items-center gap-3 flex-xl-wrap mt-4 flex-wrap">
-          <!-- Kode kalian untuk perulangan -->
+          <!-- Kode kalian untuk looping -->
           <?php // Kode Kalian Disini ?>
           <!-- ---------------------------- -->
 
@@ -35,10 +35,10 @@ include("list_menu.php");
                     style="display:flex; justify-content: center; margin: auto; margin: 20px; align-items: center; width: 15rem;"><?php include("image.php") ?></span>
                <div class="card-body">
 
-                    <!-- Kode kalian untuk perulangan -->
+                    <!-- Kode kalian untuk looping -->
                     <h5 class="card-title"><?= // Kode Kalian Disini ?></h5>
                     <!-- ---------------------------- -->
-                    <!-- Kode kalian untuk perulangan -->
+                    <!-- Kode kalian untuk looping -->
                     <p class="card-text">Rp. <?= // Kode Kalian Disini ?></p>
                     <!-- ---------------------------- -->
 
